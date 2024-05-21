a11b8868d6d2675f38a17eb829d6a89e9a9e1de8
diexban <166546790+diexban@users.noreply.github.com>
2024-05-21 15:02:12 -0600

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -5,20 +5,15 @@ create_user() {
     username=$1
     password=$2
 
-    
     sudo useradd -m -s /bin/bash $username
 
-   
     echo "$username:$password" | sudo chpasswd
 
-   
     echo "User '$username' has been created."
 }
-
 
 read -p "Enter username: " username
 read -sp "Enter password: " password
 echo
-
 
 create_user $username $password
